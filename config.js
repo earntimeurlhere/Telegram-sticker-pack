34891e9004b8f54a9332f6e3066fd7e37f9b4c03
earntimeurlhere <earntimeurl@gmail.com>
2026-06-14 10:47:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  BOT_TOKEN: "YOUR_BOT_TOKEN_HERE",
-  BOT_USERNAME: "Movie_hubbbbbbot",
+  BOT_TOKEN: "8667702235:AAEWyjE6cHEmQMm_ueRKUBdVGdZzO7jAbas",
+  BOT_USERNAME: "Stickorabot",
 
   BUNDLES_FILE: "bundles.txt",
   USERS_FILE: "users.txt"
